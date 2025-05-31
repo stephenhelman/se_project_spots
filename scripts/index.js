5522883a6cfc3cb4421510eb3b918699c29881c8
stephenhelman <stephenhelman18@gmail.com>
2025-05-31 11:23:21 -0600

    Preview modal dynamic rendering functionality

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
   },
   {
-    name: "A very long bridge, over the forest and through the trees",
+    name: "A very long bridge",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
   },
   {
@@ -23,13 +23,17 @@ const initialCards = [
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
+  {
+    name: "Golden Gate Bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
 ];
 
 //selecting elements that control the edit profile modal
 const editProfileButton = document.querySelector(".profile__button-edit");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
-  ".modal__close-button"
+  ".modal__button_type_close"
 );
 
 //selecting the parts of the form that will be prefilled
@@ -47,16 +51,50 @@ const profileJobElement = profile.querySelector(".profile__description");
 //selection elements that control the new post modal
 const newPostButton = document.querySelector(".profile__button-new");
 const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+const newPostCloseButton = newPostModal.querySelector(
+  ".modal__button_type_close"
+);
 
-//select the parts of the new post modal to log
+//select the parts of the new post modal to use
 const addCardFormElement = newPostModal.querySelector(".modal__form");
 const nameInput = addCardFormElement.querySelector("#caption-input");
 const linkInput = addCardFormElement.querySelector("#image-link-input");
 
+//select the parts of the image preview modal
+const previewModal = document.querySelector("#preview-modal");
+const previewCloseButton = previewModal.querySelector(
+  ".modal__button_type_preview-close"
+);
+const modalImage = previewModal.querySelector(".modal__image");
+const modalCaption = previewModal.querySelector(".modal__caption");
+
+//select the card template and cards container
 const cardsContainer = document.querySelector(".cards__list");
 const cardTemplate = document.querySelector("#card-template");
 
+//helper functions
+const prefillForm = (text, formElement) => {
+  formElement.value = text;
+};
+
+const changeText = (value, elementToChange) => {
+  elementToChange.textContent = value;
+};
+
+const openModal = (modal) => {
+  modal.classList.add("modal_is-opened");
+};
+
+const closeModal = (modal) => {
+  modal.classList.remove("modal_is-opened");
+};
+
+const setImageAttributes = (imageElement, data) => {
+  imageElement.setAttribute("src", data.link);
+  imageElement.setAttribute("alt", data.name);
+};
+
+//create a new card
 const getCardElement = (data) => {
   const cardElement = cardTemplate.content
     .querySelector(".card")
@@ -67,7 +105,7 @@ const getCardElement = (data) => {
   const likeButton = cardElement.querySelector(".card__like-button");
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
-  cardTitle.textContent = data.name;
+  changeText(data.name, cardTitle);
   cardImage.setAttribute("src", data.link);
   cardImage.setAttribute("alt", data.name);
 
@@ -79,29 +117,19 @@ const getCardElement = (data) => {
     cardElement.remove();
   });
 
+  cardImage.addEventListener("click", () => {
+    setImageAttributes(modalImage, data);
+    changeText(data.name, modalCaption);
+    openModal(previewModal);
+  });
+
   return cardElement;
-};
-
-const prefillForm = (text, formElement) => {
-  formElement.value = text;
-};
-
-const changePageText = (inputToUse, elementToChange) => {
-  elementToChange.textContent = inputToUse;
-};
-
-const openModal = (modal) => {
-  modal.classList.add("modal_is-opened");
-};
-
-const closeModal = (modal) => {
-  modal.classList.remove("modal_is-opened");
 };
 
 const handleProfileFormSubmission = (e) => {
   e.preventDefault();
-  changePageText(profileNameInput.value, profileNameElement);
-  changePageText(jobInput.value, profileJobElement);
+  changeText(profileNameInput.value, profileNameElement);
+  changeText(jobInput.value, profileJobElement);
   closeModal(editProfileModal);
 };
 
@@ -118,14 +146,14 @@ const handleNewPostFormSubmission = (e) => {
 };
 
 //open the edit profile modal on edit button click
-editProfileButton.addEventListener("click", function () {
+editProfileButton.addEventListener("click", () => {
   openModal(editProfileModal);
   prefillForm(profileNameElement.textContent, profileNameInput);
   prefillForm(profileJobElement.textContent, jobInput);
 });
 
 //close the edit profile modal on close button click
-editProfileCloseButton.addEventListener("click", function () {
+editProfileCloseButton.addEventListener("click", () => {
   closeModal(editProfileModal);
 });
 
@@ -133,17 +161,21 @@ editProfileCloseButton.addEventListener("click", function () {
 profileFormElement.addEventListener("submit", handleProfileFormSubmission);
 
 //open the new post modal on edit button click
-newPostButton.addEventListener("click", function () {
+newPostButton.addEventListener("click", () => {
   openModal(newPostModal);
 });
 
 //close the new post modal on close button click
-newPostCloseButton.addEventListener("click", function () {
+newPostCloseButton.addEventListener("click", () => {
   closeModal(newPostModal);
 });
 
 //save the changes and display them on the page
 addCardFormElement.addEventListener("submit", handleNewPostFormSubmission);
+
+previewCloseButton.addEventListener("click", () => {
+  closeModal(previewModal);
+});
 
 initialCards.forEach((card) => {
   const newCard = getCardElement(card);
